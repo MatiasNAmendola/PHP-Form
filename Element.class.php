a79a841f76dab9ebf0e76a2ecc3e26637833332c
Christophe Tafani-Dereeper <pro@christophetd.fr>
2012-01-30 17:20:29 +0100

    Adding code to Form & Element classes

--- Element.class.php
+++ Element.class.php
@@ -1,19 +1,33 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Element
+ * Element class.
  *
+ * @name Element
  * @author Christophe
  */
 class Element {
-
+    
+    protected $_name;
+    protected $_validators;
+    
     public function __construct() {
         
+    }
+    
+    public function __call($name, $arguments) {
+        $callType = substr($name, 0, 3);
+        if($callType == 'get') {
+            $var = strtolower(substr($name, 3));
+            if(isset($this->{'_'.$var})) {
+                return $this->{'_'.$var};
+            }
+        }
+        if($callType == 'set') {
+            $var = strtolower(substr($name, 3));
+            if(isset($this->{'_'.$var})) {
+                $this->{'_'.$var} = $arguments[0];
+            }
+        }
     }
 
 }
--- Form.class.php
+++ Form.class.php
@@ -6,12 +6,57 @@
  */
 
 /**
- * Description
+ * Main class
  * 
+ * @name Form
  * @author Christophe
  */
 class Form {
    
+    protected $_name;
+    protected $_action;
+    protected $_method;  
+    protected $_elements;
+    
+    const FORM_ELEMENTS_MAINCLASS           = 'Element';
+    const FORM_ELEMENTS_RENDERHTMLMETHOD    = 'render';
+    
+    public function __construct($parameters = array()) {
+        $this->_name    = isset($parameters['name'])    ?   $parameters['name']      : null;
+        $this->_action  = isset($parameters['action'])  ?   $parameters['action']    : null;
+        $this->_method  = isset($parameters['method'])  ?   $parameters['method']    : null;    
+        $this->_elements = array();
+    }
+    
+    public function add($elementObject) {
+        if(get_class($elementObject) == FORM_ELEMENTS_MAINCLASS) {
+            $this->_elements[] = $elementObject;
+        }
+        return $elementObject;
+    }
+    
+    public function render() {
+        $htmlRender = ''; /* To do, using a layout file */
+        foreach($this->_elements as $element) {
+            if(method_exists($element, FORM_ELEMENTS_RENDERHTMLMETHOD)) {
+                $htmlRender .= $element->{FORM_ELEMENTS_RENDERHTMLMETHOD}();
+            }
+        }
+        
+        /* To do */
+    }
+    
+    public function isValid() {
+        $valid = true;
+        foreach($this->_elements as $element) {
+            if(!$element->validate()) {
+                $valid = false;
+                break 2;
+            }
+        }
+        
+        return $valid;
+    }
     
 }
 
