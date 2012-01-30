04f09823fab711204166319815e47de160bc5b83
Christophe Tafani-Dereeper <pro@christophetd.fr>
2012-01-30 16:55:51 +0100

    Initial commit

---
+++ Element.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Element
+ *
+ * @author Christophe
+ */
+class Element {
+
+    public function __construct() {
+        
+    }
+
+}
+
+?>
